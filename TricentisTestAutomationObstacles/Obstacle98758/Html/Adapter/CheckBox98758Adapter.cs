0dce144634f1b19500351f91808a195aac0a3115
DARKinVADER <rrnd79@gmail.com>
2017-07-05 11:33:01 +0200

    98758 solution

---
+++ TricentisTestAutomationObstacles/Obstacle98758/Html/Adapter/CheckBox98758Adapter.cs
@@ -0,0 +1,35 @@
+﻿using Tricentis.Automation.Creation;
+using Tricentis.Automation.Engines;
+using Tricentis.Automation.Engines.Adapters;
+using Tricentis.Automation.Engines.Adapters.Attributes;
+using Tricentis.Automation.Engines.Adapters.Html.Generic;
+using Tricentis.Automation.Engines.Technicals.Html;
+
+namespace Obstacle98758.Html.Adapter
+{
+    [SupportedTechnical(typeof(IHtmlDivTechnical))]
+    public class CheckBox98758Adapter : AbstractHtmlDomNodeAdapter<IHtmlDivTechnical>, ICheckBoxAdapter
+    {
+        public CheckBox98758Adapter(IHtmlDivTechnical technical, Validator validator) : base(technical, validator)
+        {
+            validator.AssertTrue(() => technical.Id == "custom-check-box");
+        }
+
+        public string Label => string.Empty;
+
+        public CheckState Selected
+        {
+            get
+            {
+                return Technical.ClassName.Contains("unchecked") ? CheckState.False : CheckState.True;
+            }
+            set
+            {
+                Technical.Click();
+            }
+        }
+
+        public bool TriState => false;
+        public override string DefaultName => "GCCheckbox - " + base.DefaultName;
+    }
+}
